c00ef4ce863da4cbff2edd92a2ea2072269d99b2
Sergey <sergey.listratov@gmail.com>
2023-05-18 11:01:16 +0300

    rpz 18.05.23 01

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, render_template
+from werkzeug.exceptions import abort
+
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html')
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
